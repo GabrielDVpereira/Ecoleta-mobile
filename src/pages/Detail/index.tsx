47275b2d3df1ceea7cb2ae1f7a130cf4a1ad879b
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-04 23:51:20 -0300

    Detail page

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,79 +1,42 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, TouchableOpacity, Image, Text } from "react-native";
+import styles from "./styles";
+import { Feather as Icon, FontAwesome } from "@expo/vector-icons";
+import { RectButton } from "react-native-gesture-handler";
 
-const Detail = () => <View />;
+const Detail = ({ navigation }) => (
+  <>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#34cb79" />
+      </TouchableOpacity>
+
+      <Image
+        style={styles.pointImage}
+        source={{
+          uri:
+            "https://images.unsplash.com/photo-1569180880150-df4eed93c90b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+        }}
+      />
+
+      <Text style={styles.pointName}>Mercado teste</Text>
+      <Text style={styles.pointItems}>Lampadas</Text>
+
+      <View style={styles.address}>
+        <Text style={styles.addressTitle}>Endereço</Text>
+        <Text style={styles.addressContent}>Brasilia, DF</Text>
+      </View>
+    </View>
+    <View style={styles.footer}>
+      <RectButton style={styles.button} onPress={() => {}}>
+        <FontAwesome name="whatsapp" size={20} color="#fff" />
+        <Text style={styles.buttonText}>Whatsapp</Text>
+      </RectButton>
+      <RectButton style={styles.button} onPress={() => {}}>
+        <Icon name="mail" size={20} color="#fff" />
+        <Text style={styles.buttonText}>Email</Text>
+      </RectButton>
+    </View>
+  </>
+);
 export default Detail;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 32,
-    paddingTop: 20,
-  },
-
-  pointImage: {
-    width: "100%",
-    height: 120,
-    resizeMode: "cover",
-    borderRadius: 10,
-    marginTop: 32,
-  },
-
-  pointName: {
-    color: "#322153",
-    fontSize: 28,
-    fontFamily: "Ubuntu_700Bold",
-    marginTop: 24,
-  },
-
-  pointItems: {
-    fontFamily: "Roboto_400Regular",
-    fontSize: 16,
-    lineHeight: 24,
-    marginTop: 8,
-    color: "#6C6C80",
-  },
-
-  address: {
-    marginTop: 32,
-  },
-
-  addressTitle: {
-    color: "#322153",
-    fontFamily: "Roboto_500Medium",
-    fontSize: 16,
-  },
-
-  addressContent: {
-    fontFamily: "Roboto_400Regular",
-    lineHeight: 24,
-    marginTop: 8,
-    color: "#6C6C80",
-  },
-
-  footer: {
-    borderTopWidth: StyleSheet.hairlineWidth,
-    borderColor: "#999",
-    paddingVertical: 20,
-    paddingHorizontal: 32,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-
-  button: {
-    width: "48%",
-    backgroundColor: "#34CB79",
-    borderRadius: 10,
-    height: 50,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  buttonText: {
-    marginLeft: 8,
-    color: "#FFF",
-    fontSize: 16,
-    fontFamily: "Roboto_500Medium",
-  },
-});
