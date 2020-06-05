7ad75f04fadd15e62766bcbd7cb353344159871d
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-04 22:41:13 -0300

    finishing home page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { StatusBar } from "react-native";
-import Home from "./src/Home";
+import Home from "./src/pages/Home";
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
+import { AppLoading } from "expo";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
     <>
-      <StatusBar />
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Home />
     </>
   );
